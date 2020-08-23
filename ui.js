1b42ede17d0673d5fa6965629e929e98de54900d
Andrew Mushel <almushel@gmail.com>
2020-08-23 15:11:02 -0500

    Added player health bar.

---
+++ ui.js
@@ -0,0 +1,22 @@
+const HP_COLOR = '#ff6060'
+
+function drawUI() {
+    ctx.translate(30, 30)
+    drawHP();
+    ctx.translate(-30, -30);
+}
+
+function drawHP () {
+    const fontSize = w/50;
+    let left = fontSize * 2;
+    let top = 0;
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'middle';
+    ctx.font = fontSize + 'px Arial';
+    ctx.fillStyle = 'white';
+    ctx.fillText('HP', 0, 5);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(left - 2, top - 2, 204, 14);
+    ctx.fillStyle = HP_COLOR;
+    ctx.fillRect(left, top, (player.hp/100) * 200, 10);
+}
