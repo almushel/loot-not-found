adcd5c64cb6e522bd74323a40cdaf012b5485d4f
Andrew Mushel <almushel@gmail.com>
2020-08-24 18:32:30 -0500

    Improved HUD positioning/scaling

--- ui.js
+++ ui.js
@@ -1,37 +1,47 @@
 const HP_COLOR = '#ff6060'
 
 function drawUI() {
-    ctx.translate(30, 35)
+    const offset = w/25;
+    ctx.translate(offset, offset)
     drawHP();
+    ctx.translate(-offset*2, 0);
     drawLoot();
-    ctx.translate(-30, -35);
+    ctx.translate(offset, -offset);
 }
 
 function drawHP () {
-    const fontSize = w/50;
-    let left = fontSize * 2;
-    let top = 0;
+    const fontSize = w/50, barWidth = w / 5;
+    let left = fontSize * 2, top = 0;
+
     ctx.textAlign = 'left';
     ctx.textBaseline = 'middle';
     ctx.font = fontSize + 'px Arial';
     ctx.fillStyle = 'white';
+    
     ctx.fillText('HP', fontSize/2.5, top + 5);
+    
     ctx.fillStyle = 'black';
-    ctx.fillRect(left - 2, top - 2, 204, 14);
+    ctx.fillRect(left - 2, top - fontSize/4 - 2, barWidth + 4, fontSize/2 + 4);
+    
     ctx.fillStyle = HP_COLOR;
-    ctx.fillRect(left, top, (player.hp/100) * 200, 10);
+    ctx.fillRect(left, top - fontSize/4, (player.hp/100) * barWidth, fontSize/2);
 }
 
 function drawLoot() {
     const fontSize = w/50;
-    let left = w - 100 - fontSize * 2;
-    let top = 5;
+
     ctx.textAlign = 'left';
     ctx.textBaseline = 'middle';
     ctx.font = fontSize + 'px Arial';
     ctx.fillStyle = 'white';
+
+    let lootWidth = ctx.measureText('LOOT').width;
+    let numWidth = ctx.measureText(100).width;
+    let left = w - (lootWidth + numWidth + fontSize);
+    let top = 5;
+   
     ctx.fillText('LOOT', left, top);
     ctx.fillStyle = 'gold';
-    let lOffset = ctx.measureText('LOOT').width;
-    ctx.fillText(player.loot, left + lOffset + fontSize/2, top);
+    
+    ctx.fillText(player.loot, left + lootWidth + fontSize/2, top);
 }
